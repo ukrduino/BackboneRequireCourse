132d26786ee00ef44645987795459ddfff13cb68
sromanyuk <blacksun@mail.ru>
2016-04-04 15:20:36 +0300

    messages are posted and added to wall

--- public/scripts/collections/messages.js
+++ public/scripts/collections/messages.js
@@ -1,1 +1,12 @@
+define(['underscore', 'backbone', 'settings',  'messageModel'], function (_, Backbone,  settings, MessageModel) {
+
+    return Backbone.Collection.extend({
+            url: settings.get('wallPosts'),
+            model: MessageModel,
+            initialize: function () {
+            }
+        }
+    )
+});
+
 
