05cca852955f9b722be6a869d41a5874da3f1dd4
sromanyuk <blacksun@mail.ru>
2016-04-07 10:22:46 +0300

    friends Page layout fixes

--- public/scripts/views/userCardView.js
+++ public/scripts/views/userCardView.js
@@ -6,16 +6,22 @@ define(['underscore',
     'jquery',
     'backbone',
     'settings',
+    'text!../../templates/friendCard.html',
     'text!../../templates/userCard.html',
-    'bootstrap'], function (_, $, Backbone, settings, userCardTemplate) {
+    'bootstrap'], function (_, $, Backbone, settings, friendCardTemplate, userCardTemplate) {
 
     return Backbone.View.extend({
-        template: _.template(userCardTemplate),
+        userTemplate: _.template(userCardTemplate),
+        friendTemplate: _.template(friendCardTemplate),
         initialize: function () {
         },
         render: function () {
             //this.model.processDates();
-            this.$el.html(this.template(this.model.toJSON()));
+            if (this.model.get('showDeleteButton')) {
+                this.$el.html(this.friendTemplate(this.model.toJSON()));
+            } else {
+                this.$el.html(this.userTemplate(this.model.toJSON()));
+            }
             return this;
         }
     });
