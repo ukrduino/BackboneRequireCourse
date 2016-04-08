f4aadfe2fc4f61033dca21a818248df644ea136f
sromanyuk <blacksun@mail.ru>
2016-04-08 12:27:46 +0300

    refactoring

---
+++ public/scripts/views/mainPageView.js
@@ -0,0 +1,38 @@
+/**
+ *    Main Page View
+ */
+
+define(['underscore',
+    'jquery',
+    'backbone',
+    'sidePanelView',
+    'messagesPanelView'], function (_, $, Backbone, SidePanelView, MessagesPanelView) {
+
+    return Backbone.View.extend({
+
+        initialize: function () {
+            this.childViews = [];
+        },
+
+        onClose: function () {
+            // unbind all events from models, collections here!!!
+            //https://lostechies.com/derickbailey/2011/09/15/zombies-run-managing-page-transitions-in-backbone-apps/
+            console.log('Main Page view onClose');
+
+            // colling close() on all subviews
+            this.childViews.forEach(function (view) {
+                view.close();
+            });
+        },
+
+        render: function () {
+            /*
+             * adding our child views in childView array
+             */
+            this.childViews[0] = new SidePanelView();
+            this.childViews[1] = new MessagesPanelView();
+        }
+    });
+});
+
+
