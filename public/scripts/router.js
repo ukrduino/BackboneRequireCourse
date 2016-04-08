2cff13913ebd8b3872db1b24438bf1837b296bb6
sromanyuk <blacksun@mail.ru>
2016-04-08 16:31:11 +0300

    showView() method

--- public/scripts/router.js
+++ public/scripts/router.js
@@ -7,18 +7,25 @@ define([
     'loggedInUser',
     'invitationView',
     'headerView',
-    'sidePanelView',
-    'messagesPanelView',
+    'mainPageView',
     'loginView',
     'registrationView',
     'editProfileView',
     'profilePageView',
-    'myFriendsPanelView',
-    'searchUserPanelView',
-    'text!../../templates/homePageBaseTemplate.html',
-    'text!../../templates/friendsPageBaseTemplate.html'
-], function ($, _, Backbone, Settings, eventDispatcher, LoggedInUser, InvitationView, HeaderView, SidePanelView, MessagePanelView, LoginView,
-             RegistrationView, EditProfileView, ProfilePageView, MyFriendsPanelView, SearchUsersPanelView, homePageBaseTemplate,friendsPageBaseTemplate) {
+    'userSearchPageView'], function ($,
+                                     _,
+                                     Backbone,
+                                     Settings,
+                                     eventDispatcher,
+                                     LoggedInUser,
+                                     InvitationView,
+                                     HeaderView,
+                                     MainPageView,
+                                     LoginView,
+                                     RegistrationView,
+                                     EditProfileView,
+                                     ProfilePageView,
+                                     UserSearchPageView) {
     var AppRouter = Backbone.Router.extend({
         routes: {
             '': 'showHomePage',
@@ -30,48 +37,49 @@ define([
             'friends': 'showFriendsPage'
         },
         userIsLoggedIn: function () {
+            console.log('LoggedInUser:', LoggedInUser);
             return !_.isNull(LoggedInUser.get('id')) && !_.isUndefined(LoggedInUser.get('id'))
+        },
+        showView: function (view) {
+            console.log('CurrentView:', this.currentView);
+            if (!_.isUndefined(this.currentView)) {
+                this.currentView.close();
+            }
+            this.currentView = view;
+            return view;
         }
     });
 
     var initialize = function () {
         var app_router = new AppRouter;
         app_router.on('route:showLoginForm', function () {
-            var loginView = new LoginView();
-            loginView.render();
+            this.showView(new LoginView()).render();
         });
         app_router.on('route:showRegistrationForm', function () {
-            var registrationView = new RegistrationView();
-            registrationView.render();
+            this.showView(new RegistrationView()).render();
         });
         app_router.on('route:showProfilePage', function () {
             if (this.userIsLoggedIn()) {
-                var profilePageView = new ProfilePageView();
                 var userDataJson = LoggedInUser.toJSON();
                 userDataJson['loggedInUser'] = true;
-                profilePageView.preRender(userDataJson);
+                this.showView(new ProfilePageView()).preRender(userDataJson);
             }
         });
         app_router.on('route:showEditProfileForm', function () {
             if (this.userIsLoggedIn()) {
-                var editProfileView = new EditProfileView();
-                editProfileView.render();
+                this.showView(new EditProfileView()).render();
             }
         });
         app_router.on('route:showFriendsPage', function () {
             if (this.userIsLoggedIn()) {
-                $('#contentBlock').empty().append(_.template(friendsPageBaseTemplate));
-                new MyFriendsPanelView();
-                new SearchUsersPanelView();
+                this.showView(new UserSearchPageView()).render();
             }
         });
         app_router.on('route:showHomePage', function () {
             if (!this.userIsLoggedIn()) {
-                eventDispatcher.trigger('InvitationView:render');
+                this.showView(new InvitationView()).render();
             } else {
-                $('#contentBlock').empty().append(_.template(homePageBaseTemplate));
-                new SidePanelView();
-                new MessagePanelView();
+                this.showView(new MainPageView()).render();
             }
         });
         app_router.on('route:logOut', function () {
@@ -83,15 +91,14 @@ define([
                     data: {api_key: Settings.get('apiKey')},
                     type: 'DELETE',
                     success: function (result) {
-                        Backbone.history.navigate('', {trigger: true});
+                        Backbone.history.navigate('login', {trigger: true});
                     }
                 });
             }
         });
         app_router.listenTo(eventDispatcher, 'router:showUserProfilePage', function (userDataJson) {
             if (this.userIsLoggedIn()) {
-                var profilePageView = new ProfilePageView();
-                profilePageView.preRender(userDataJson);
+                this.showView(new ProfilePageView()).preRender(userDataJson);
             }
         });
 
