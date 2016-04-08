6e5b72d970c0e1c021f9293b2452aff935d007f0
sromanyuk <blacksun@mail.ru>
2016-04-08 12:24:51 +0300

    onClose added

--- public/scripts/views/sidePanelView.js
+++ public/scripts/views/sidePanelView.js
@@ -10,18 +10,23 @@ define(['underscore',
     'bootstrap'], function (_, $, Backbone, LoggedInUser, sidePanelTemplate) {
 
     return Backbone.View.extend({
-        el: '#sidePanel',
+        id: 'sidePanel',
         template: _.template(sidePanelTemplate),
         initialize: function () {
+            console.log("sidePanel view render");
             this.render();
+        },
+        onClose: function () {
+            // unbind all events from models, collections here!!!
+            //https://lostechies.com/derickbailey/2011/09/15/zombies-run-managing-page-transitions-in-backbone-apps/
+            console.log('sidePanel view onClose');
         },
 
         render: function () {
-            this.$el.empty();
-            this.$el.html(this.template(LoggedInUser.toJSON()));
+            console.log("sidePanel view render");
+            $('#contentBlock').append(this.$el.html(this.template(LoggedInUser.toJSON())));
             // enable Bootstrap tooltips after rendering
             $('[data-toggle="tooltip"]').tooltip();
-            return this;
         }
     });
 });
