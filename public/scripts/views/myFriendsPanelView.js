9ddbb4a8a4be7bd6df69670b4e19b51dc257c58b
sromanyuk <blacksun@mail.ru>
2016-04-11 13:50:15 +0300

    refactoring

--- public/scripts/views/myFriendsPanelView.js
+++ public/scripts/views/myFriendsPanelView.js
@@ -7,17 +7,21 @@ define(['underscore',
     'backbone',
     'settings',
     'eventDispatcher',
-    'userModel',
     'loggedInUser',
-    'friendsCollection',
     'userCardView',
     'text!../../templates/myFriendsPanel.html',
-    'bootstrap'], function (_, $, Backbone, settings, eventDispatcher, UserModel, LoggedInUser, FriendsCollection, UserCardView, myFriendsPanelTemplate) {
+    'bootstrap'], function (_,
+                            $,
+                            Backbone,
+                            settings,
+                            eventDispatcher,
+                            LoggedInUser,
+                            UserCardView,
+                            myFriendsPanelTemplate) {
 
     return Backbone.View.extend({
         id: 'friendsPanel',
         template: _.template(myFriendsPanelTemplate),
-        collection: new FriendsCollection(),
         events: {
             "click .removeFriend": 'removeFriend',
             "click .viewProfile": 'viewProfile'
@@ -30,62 +34,38 @@ define(['underscore',
         },
         initialize: function () {
             this.render();
-            this.fetchCollection();
-            this.listenTo(eventDispatcher, 'userModel:successAddToFriends userModel:successRemoveFromFriend', function () {
-                this.fetchCollection();
+            this.showFriends();
+            this.listenTo(eventDispatcher, 'LoggedInUser:friendsCollectionUpdated', function () {
+                this.showFriends();
             });
         },
         render: function () {
-            console.log("Users Search panel render");
+            if(!_.isUndefined(LoggedInUser.collection)){
+                LoggedInUser.getFriends();
+            }
             $('#contentBlock').append(this.$el.html(this.template));
         },
 
-        fetchCollection: function () {
-            var that = this;
-            console.log("Friends panel collection fetch");
-            this.collection.fetch({
-                data: {api_key: settings.get('apiKey')},
-                reset: true,
-                success: function (collection, response, options) {
-                    console.log('Friends collection fetch success');
-                    that.showFriends();
-                },
-                error: function (collection, response, options) {
-                    console.log('error');
-                }
-            });
-        },
-
         showFriends: function () {
-            var that = this;
             $('#friends').empty();
-            this.collection.each(function (userModel) {
+            LoggedInUser.collection.each(function (userModel) {
                     userModel.set('isFriend', true).set('showDeleteButton', true);
-                    that.addUserCardToFriendsPanel(userModel)
+                    var userCardView = new UserCardView({model: userModel});
+                    $('#friends').append(userCardView.render().el);
                 }
             );
             // enable Bootstrap tooltips after rendering
             $('[data-toggle="tooltip"]').tooltip();
         },
 
-        addUserCardToFriendsPanel: function (userModel) {
-            var userCardView = new UserCardView({model: userModel});
-            $('#friends').append(userCardView.render().el);
-        },
-
         removeFriend: function (event) {
             var user_id = $(event.currentTarget).data('id');
-            console.log('removeFriend :', user_id);
-            var user = new UserModel();
-            user.removeFromFriend(user_id);
+            LoggedInUser.removeFromFriend(user_id);
         },
 
         viewProfile: function (event) {
             var user_id = $(event.currentTarget).data('id');
             Backbone.history.navigate('#profile/' + user_id, {trigger: true});
-            var userDataJson = this.collection.get(user_id).toJSON();
-            userDataJson['loggedInUser'] = false;
-            eventDispatcher.trigger('router:showUserProfilePage', userDataJson);
         }
     });
 });
