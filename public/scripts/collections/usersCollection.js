c175aa7d76199052f9c7f225af67a5e400cb9f14
sromanyuk <blacksun@mail.ru>
2016-04-11 10:38:26 +0300

    refactoring

--- public/scripts/collections/usersCollection.js
+++ public/scripts/collections/usersCollection.js
@@ -1,7 +1,7 @@
 define(['underscore', 'backbone', 'settings',  'userModel'], function (_, Backbone,  settings, UserModel) {
 
     return Backbone.Collection.extend({
-            url: settings.get('getUsers'),
+            url: settings.get('usersUrl'),
             model: UserModel,
             initialize: function () {
             }
