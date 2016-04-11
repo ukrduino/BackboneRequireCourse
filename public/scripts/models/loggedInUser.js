c9d369e0e946c02eaeddfbad4a012f1a84116534
sromanyuk <blacksun@mail.ru>
2016-04-11 08:58:38 +0300

    refactoring

--- public/scripts/models/loggedInUser.js
+++ public/scripts/models/loggedInUser.js
@@ -1,5 +1,11 @@
-define(['userModel'],
-    function (UserModel) {
+define(['underscore',
+        'jquery',
+        'backbone',
+        'settings',
+        'eventDispatcher',
+        'userModel',
+        'friendsCollection'],
+    function (_, $, Backbone, settings, eventDispatcher, UserModel, FriendsCollection) {
         var loggedInUser = UserModel.extend({
             setUserData: function (responseJson) {
                 if (typeof(Storage) !== "undefined") {
@@ -8,6 +14,65 @@ define(['userModel'],
                     console.log('Sorry! No Web Storage support..');
                 }
                 this.set(responseJson);
+                return this;
+            },
+            collection: new FriendsCollection(),
+
+            initialize: function () {
+                this.listenTo(this.collection, 'reset', function () {
+                    eventDispatcher.trigger('LoggedInUser:friendsCollectionUpdated')
+                });
+                this.listenTo(eventDispatcher, 'userModel:successRemoveFromFriend', function () {
+                    this.getFriends();
+                });
+                this.listenTo(eventDispatcher, 'userModel:successAddToFriends', function () {
+                    this.getFriends();
+                })
+            },
+            getFriends: function () {
+                this.collection.fetch({
+                    data: {api_key: settings.get('apiKey')},
+                    reset: true,
+                    success: function (collection, response, options) {
+                        console.log('Friends collection fetch success', collection);
+                    },
+                    error: function (collection, response, options) {
+                        console.log('error');
+                    }
+                })
+            },
+            removeFromFriend: function (user_id) {
+                var data = {api_key: settings.get('apiKey')};
+                $.ajax({
+                    url: settings.get('removeFriend') + user_id,
+                    data: data,
+                    type: 'DELETE',
+                    success: function () {
+                        console.log('removeFriend success');
+                        eventDispatcher.trigger('userModel:successRemoveFromFriend');
+                    },
+                    error: function () {
+                        console.log('removeFriend error');
+                    }
+                });
+            },
+            addToFriends: function (user_id) {
+                var data = {api_key: settings.get('apiKey')};
+                data['user_id'] = user_id;
+                $.ajax({
+                    url: settings.get('addFriend'),
+                    data: data,
+                    type: 'POST',
+                    success: function () {
+                        eventDispatcher.trigger('userModel:successAddToFriends');
+                    },
+                    error: function () {
+                        console.log('add_friend error');
+                    }
+                });
+            },
+            checkUserIsFriend: function (user_id) {
+                return !_.isUndefined(this.collection.get(user_id))
             }
         });
         return new loggedInUser();
