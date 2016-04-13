e9796e22ceb8ce271e5a9334d99095a9ec2c737c
sromanyuk <blacksun@mail.ru>
2016-04-13 17:06:15 +0300

    Friends filter finished

--- public/scripts/collections/messagesCollection.js
+++ public/scripts/collections/messagesCollection.js
@@ -1,7 +1,7 @@
 define(['underscore', 'backbone', 'settings',  'messageModel'], function (_, Backbone,  settings, MessageModel) {
 
     return Backbone.Collection.extend({
-            url: settings.get('wallPosts'),
+            url: settings.get('searchWallPosts'),
             model: MessageModel,
             initialize: function () {
             }
