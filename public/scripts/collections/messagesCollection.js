060a47b376d92fcf3a00ff843b266ab1ac8caf12
sromanyuk <blacksun@mail.ru>
2016-04-13 18:47:59 +0300

    Users message wall added

--- public/scripts/collections/messagesCollection.js
+++ public/scripts/collections/messagesCollection.js
@@ -1,9 +1,12 @@
-define(['underscore', 'backbone', 'settings',  'messageModel'], function (_, Backbone,  settings, MessageModel) {
+define(['underscore', 'backbone', 'settings', 'messageModel'], function (_, Backbone, settings, MessageModel) {
 
     return Backbone.Collection.extend({
             url: settings.get('searchWallPosts'),
             model: MessageModel,
             initialize: function () {
+            },
+            comparator: function (model) {
+                return -new Date(model.get('created_at'));
             }
         }
     )
